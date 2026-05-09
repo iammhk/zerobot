9cf2022d7bb0ef8e5cbe9f29698f96c32ffd8a09
iammhk <mohakshukla.in@gmail..com>
2026-05-09 22:33:06 +0530

    feat: add high-variety idle gestures (shrugs, shifts, bounces)

--- scripts/mvmt_idle.py
+++ scripts/mvmt_idle.py
@@ -1,5 +1,5 @@
-# scripts/mvmt_idle.py - Subtle idle movements for Zerobot
-# This file is used to make the robot feel "alive" while waiting for commands.
+# scripts/mvmt_idle.py - High-variety idle movements for Zerobot
+# This file provides multiple random "life-like" gestures for the robot.
 
 import time
 import random
@@ -9,33 +9,54 @@ import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from zerobot import servo
 
-def run():
-    """Executes a more prominent, noticeabe idle twitch."""
-    # Choose 2 or 3 random legs to move more significantly
+def leg_twitch():
+    """Twitches 2-3 random legs."""
     legs = random.sample([servo.L3, servo.R3, servo.L4, servo.R4], random.randint(2, 3))
-    
-    # More prominent offset (25-35 degrees)
-    offset = random.randint(25, 40)
-    
+    offset = random.randint(25, 45)
     for leg in legs:
         if leg in [servo.L3, servo.R4]: # Home 0
             servo.set_angle(leg, offset)
         else: # Home 180
             servo.set_angle(leg, 180 - offset)
-            
     time.sleep(0.2)
-    
-    # Small jitter before returning
-    for leg in legs:
-        if leg in [servo.L3, servo.R4]:
-            servo.set_angle(leg, offset - 10)
-        else:
-            servo.set_angle(leg, 180 - offset + 10)
-    
-    time.sleep(0.15)
-    
-    # Return to home
     servo.move_to_home()
+
+def body_shift():
+    """Shifts body weight left or right using shoulders."""
+    side = random.choice([-1, 1])
+    offset = 20 * side
+    # L1, L2 are on one side; R1, R2 on the other
+    # Home: L1=45, R1=135, L2=135, R2=45
+    servo.set_angle(servo.L1, 45 + offset)
+    servo.set_angle(servo.L2, 135 + offset)
+    servo.set_angle(servo.R1, 135 + offset)
+    servo.set_angle(servo.R2, 45 + offset)
+    time.sleep(0.3)
+    servo.move_to_home()
+
+def shoulder_shrug():
+    """Small shrug with all shoulder servos."""
+    offset = 15
+    servo.set_angle(servo.L1, 45 + offset)
+    servo.set_angle(servo.R1, 135 - offset)
+    servo.set_angle(servo.L2, 135 - offset)
+    servo.set_angle(servo.R2, 45 + offset)
+    time.sleep(0.2)
+    servo.move_to_home()
+
+def knee_bounce():
+    """Quick micro-bounce on all legs."""
+    offset = 20
+    for leg in [servo.L3, servo.R3, servo.L4, servo.R4]:
+        if leg in [servo.L3, servo.R4]: servo.set_angle(leg, offset)
+        else: servo.set_angle(leg, 180 - offset)
+    time.sleep(0.15)
+    servo.move_to_home()
+
+def run():
+    """Randomly selects one of the idle variations."""
+    choice = random.choice([leg_twitch, body_shift, shoulder_shrug, knee_bounce])
+    choice()
     time.sleep(0.1)
 
 if __name__ == "__main__":
